b0474b91018702eb9cb2b5c53de59f81f5e4541d
Mansahaj <msspopli@gmail.com>
2026-04-20 01:14:15 -0700

    implemented worker and qeue

---
+++ app/worker.py
@@ -0,0 +1,13 @@
+from rq import Worker
+
+from app.task_queue import DOCUMENT_PROCESSING_QUEUE, get_redis_connection
+
+
+def main() -> None:
+    connection = get_redis_connection()
+    worker = Worker([DOCUMENT_PROCESSING_QUEUE], connection=connection)
+    worker.work()
+
+
+if __name__ == "__main__":
+    main()
